d6e683e7a289e1e61441960e561d0d6d5eecd82e
Simon Konkov <sweetsixteen.sudo@gmail.com>
2020-11-10 23:40:31 +0300

    freemarker version changed, direct input annotation reading in ftl added

--- src/main/java/generator/HtmlProcessor.java
+++ src/main/java/generator/HtmlProcessor.java
@@ -10,59 +10,34 @@ import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
-import javax.tools.Diagnostic;
 import java.io.*;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @AutoService(Processor.class)
 @SupportedAnnotationTypes("generator.HtmlForm")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class HtmlProcessor extends AbstractProcessor {
-    private static final String TYPE_PREFIX = "type_";
-    private static final String NAME_PREFIX = "name_";
-    private static final String PLACEHOLDER_PREFIX = "placeholder_";
 
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_28);
         String resultPath;
-        HtmlInput htmlInputAnnotation;
         HtmlForm htmlFormAnnotation;
-        Template template;
-        StringWriter result;
-        Path out;
-        Element currentElement;
-
         Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(HtmlForm.class);
-        String directPath = HtmlProcessor.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        directPath = File.separator + directPath;
+        String directPath = File.separator + HtmlProcessor.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         for (Element element : annotatedElements) {
-            Map<String, String> parametersMap = new HashMap<>();
+            Map<String, Object> parametersMap = new HashMap<>();
             resultPath = directPath.substring(1) + element.getSimpleName().toString().toLowerCase() + ".html";
-            out = Paths.get(resultPath);
-            try {
-                Configuration configuration = configureFreemarker(directPath);
-                result = new StringWriter();
-                BufferedWriter writer = new BufferedWriter(new FileWriter(out.toFile()));
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(resultPath).toFile()))) {
+                configureFreemarker(directPath, cfg);
                 htmlFormAnnotation = element.getAnnotation(HtmlForm.class);
                 parametersMap.put("action", htmlFormAnnotation.action());
                 parametersMap.put("method", htmlFormAnnotation.method());
                 List<? extends Element> elements = element.getEnclosedElements();
-                for (int i = 0; i < elements.size(); i++) {
-                    currentElement = elements.get(i);
-                    if ((htmlInputAnnotation = currentElement.getAnnotation(HtmlInput.class)) != null) {
-                        parametersMap.put(TYPE_PREFIX + i, htmlInputAnnotation.type());
-                        parametersMap.put(NAME_PREFIX + i, htmlInputAnnotation.name());
-                        parametersMap.put(PLACEHOLDER_PREFIX + i, htmlInputAnnotation.placeholder());
-                    }
-                }
-                template = configuration.getTemplate(element.getSimpleName().toString().toLowerCase() + ".ftl");
-                template.process(parametersMap, result);
-                writer.write(result.toString());
-                writer.close();
+                parametersMap.put("inputs", elements.stream().filter(elem -> elem.getAnnotation(HtmlInput.class) != null)
+                        .map(elem -> elem.getAnnotation(HtmlInput.class)).collect(Collectors.toList()));
+                cfg.getTemplate(element.getSimpleName().toString().toLowerCase() + ".ftl").process(parametersMap, writer);
             } catch (IOException | TemplateException e) {
                 throw new IllegalArgumentException(e);
             }
@@ -70,10 +45,8 @@ public class HtmlProcessor extends AbstractProcessor {
         return true;
     }
 
-    private Configuration configureFreemarker(String path) throws IOException {
-        Configuration cfg = new Configuration(Configuration.VERSION_2_3_30);
+    private void configureFreemarker(String path, Configuration cfg) throws IOException {
         cfg.setDirectoryForTemplateLoading(new File(path));
         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
-        return cfg;
     }
 }
--- target/classes/user.ftl
+++ target/classes/user.ftl
@@ -1,7 +1,5 @@
-
 <form method="${method}" action="${action}">
-    <input type="${type_1}" name="${name_1}" placeholder="${placeholder_1}">
-    <input type="${type_2}" name="${name_2}" placeholder="${placeholder_2}">
-    <input type="${type_3}" name="${name_3}" placeholder="${placeholder_3}">
-    <input type="${type_4}" name="${name_4}" placeholder="${placeholder_4}">
+    <#list inputs as input>
+        <input type="${input.type()}" name="${input.name()}" placeholder="${input.placeholder()}">
+    </#list>
 </form>
